e7cde1c4127987e6e48a848179bceee5881846ea
Davit Machurishvili <125227660+Davit2605@users.noreply.github.com>
2025-04-03 21:17:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fill = document.querySelector(".fill");
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
-empties.forEac((empty) => {
+empties.forEach((empty) => {
   empty.addEventListener("dragover", dragOver);
   empty.addEventListener("dragenter", dragEnter);
   empty.addEventListener("dragleave", dragLeave);
